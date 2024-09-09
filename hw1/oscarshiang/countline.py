0048f0ae98c4172c4833fb19aeb6883471390c94
Oscar Shiang <oscar0225@livemail.tw>
2024-09-09 09:37:51 +0800

    Switch python executable to use for countline.py
    Use PYTHON_BIN to specify the path of the executable for the
    script to use.

---
+++ hw1/oscarshiang/countline.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ "$PYTHON_BIN" == "" ]]; then
+    echo "Specify python to use with PYTHON_BIN"
+    exit -1
+fi
+
+exec $PYTHON_BIN - "$@" << EOF
+
+import sys
+import os.path
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
+
+EOF

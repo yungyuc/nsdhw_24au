66bea8b17bbb70d4ca78b1adf397441793bb8785
ExplorerRay <bjhuang@cs.nycu.edu.tw>
2024-09-13 07:06:43 +0000

    feat: bash script using env to specify python version

---
+++ hw1/ExplorerRay/countline.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+read -d '' CODE <<- END_OF_CODE
+import sys
+import os.path
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
+END_OF_CODE
+
+
+exec "$PYTHON_BIN" -c "$CODE" $1
+

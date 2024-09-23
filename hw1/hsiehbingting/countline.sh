f042ceec7d079a81a43724e62c1aa1074e337450
hsiehbingting <hsiehbingting@gmail.com>
2024-09-23 21:38:45 +0800

    add: countline.sh

---
+++ hw1/hsiehbingting/countline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+argv=$#
+if [ $argv -lt 1 ]; then
+    echo "missing file name"
+elif [ $argv -gt 1 ]; then
+    echo "only one argument is allowed"
+else
+    fname="$1"
+    if [ -f "$fname" ]; then
+        lines=$(wc -l < "$fname")
+        echo "$lines lines in $fname"
+    else
+        echo "$fname not found"
+    fi
+fi

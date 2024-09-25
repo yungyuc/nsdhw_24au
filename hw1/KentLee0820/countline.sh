965bbcd7c218e6e8765183ddf884f2d950eb0182
kent lee <kentlee0820@gmail.com>
2024-09-25 20:40:43 +0800

    hw1 added countline.py, countline.sh

---
+++ hw1/KentLee0820/countline.sh
@@ -0,0 +1,16 @@
+##!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "error: wrong argument"
+else
+    filename="$1"
+    if [ -f "$filename" ]; then
+        line_count=$(wc -l < "$filename")
+        echo "$line_count"
+    else
+        echo "error: $filename not found"
+    fi
+fi
+
+
+

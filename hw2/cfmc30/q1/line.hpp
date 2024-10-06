11f98a1e03d1910d2feda0d82088451b967a45c8
cfmc30 <tedted30@hotmail.com>
2024-10-06 16:51:43 +0800

    q1 init

---
+++ hw2/cfmc30/q1/line.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+class Line {
+public:
+  Line();
+  Line(Line const &);
+  Line(Line &&);
+  Line &operator=(Line const &);
+  Line &operator=(Line &&);
+  Line(size_t size);
+  ~Line();
+  size_t size() const;
+  float const &x(size_t it) const;
+  float &x(size_t it);
+  float const &y(size_t it) const;
+  float &y(size_t it);
+
+private:
+  // Member data.
+  std::vector<float> _x, _y;
+}; /* end class Line */

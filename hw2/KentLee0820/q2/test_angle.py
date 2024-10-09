19bcebd37b247b0451746e42fb49cad8a93db9d0
kent lee <kentlee0820@gmail.com>
2024-10-09 20:02:57 +0800

    file properties fixed

--- hw2/KentLee0820/q2/test_angle.py
+++ hw2/KentLee0820/q2/test_angle.py
@@ -17,7 +17,9 @@ def test_perpencular_angle():
 
 
 def test_any_angle():
-	x1, y1, x2, y2 = random.random(), random.random(), random.random(), random.random()
-	ans = math.atan2(y2, x2) - math.atan2(y1, x1)
-	ans = math.pi * 2 + ans if ans < 0 else math.pi * 2 - ans if ans > math.pi else ans 
+	x1, y1, x2, y2 = 3.0, 4.0, 5.0, 6.0
+	l1_len = math.sqrt(x1 * x1 + y1 * y1)
+	l2_len = math.sqrt(x2 * x2 + y2 * y2)
+	
+	ans = math.acos(((x1 * x2) + (y1 * y2)) / (l1_len * l2_len))
 	assert math.isclose(calculate_angle(x1, y1, x2, y2), ans, rel_tol=1e-9)

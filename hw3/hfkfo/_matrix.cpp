f6f68ba516eca5b195bf9279f6ae24dd382b80dd
hfkfo <kevinpan0930>
2024-10-17 11:44:39 +0800

    change _matrix.cpp

--- hw3/hfkfo/_matrix.cpp
+++ hw3/hfkfo/_matrix.cpp
@@ -16,63 +16,6 @@ public:
         reset_buffer(nrow, ncol);
     }
 
-    Matrix(size_t nrow, size_t ncol, std::vector<double> const & vec)
-      : m_nrow(nrow), m_ncol(ncol)
-    {
-        reset_buffer(nrow, ncol);
-        (*this) = vec;
-    }
-
-    Matrix & operator=(std::vector<double> const & vec)
-    {
-        if (size() != vec.size())
-        {
-            throw std::out_of_range("number of elements mismatch");
-        }
-
-        size_t k = 0;
-        for (size_t i=0; i<m_nrow; ++i)
-        {
-            for (size_t j=0; j<m_ncol; ++j)
-            {
-                (*this)(i,j) = vec[k];
-                ++k;
-            }
-        }
-
-        return *this;
-    }
-
-    Matrix(Matrix const & other)
-      : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
-    {
-        reset_buffer(other.m_nrow, other.m_ncol);
-        for (size_t i=0; i<m_nrow; ++i)
-        {
-            for (size_t j=0; j<m_ncol; ++j)
-            {
-                (*this)(i,j) = other(i,j);
-            }
-        }
-    }
-
-    Matrix & operator=(Matrix const & other)
-    {
-        if (this == &other) { return *this; }
-        if (m_nrow != other.m_nrow || m_ncol != other.m_ncol)
-        {
-            reset_buffer(other.m_nrow, other.m_ncol);
-        }
-        for (size_t i=0; i<m_nrow; ++i)
-        {
-            for (size_t j=0; j<m_ncol; ++j)
-            {
-                (*this)(i,j) = other(i,j);
-            }
-        }
-        return *this;
-    }
-
     Matrix(Matrix && other)
       : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
     {
@@ -110,12 +53,23 @@ public:
     size_t ncol() const { return m_ncol; }
 
     size_t size() const { return m_nrow * m_ncol; }
-    double buffer(size_t i) const { return m_buffer[i]; }
-    std::vector<double> buffer_vector() const
+    double* buffer() const { return m_buffer; }
+
+    bool operator == (Matrix const &mat1)
     {
-        return std::vector<double>(m_buffer, m_buffer+size());
+        if (this->m_nrow != mat1.nrow() || this->m_ncol != mat1.ncol())
+            return false;
+        for (size_t i = 0; i < mat1.nrow(); ++i)
+        {
+            for (size_t j = 0; j < mat1.ncol(); ++j)
+            {
+                if (this->m_buffer[index(i, j)] != mat1(i, j))
+                    return false;
+            }
+        }
+        return true;
     }
-    double * m_buffer = nullptr;
+    
 
 private:
 
@@ -128,7 +82,10 @@ private:
     {
         if (m_buffer) { delete[] m_buffer; }
         const size_t nelement = nrow * ncol;
-        if (nelement) { m_buffer = new double[nelement]; }
+        if (nelement) { 
+            m_buffer = new double[nelement]; 
+            std::fill(m_buffer, m_buffer + nelement, 0.0);
+        }
         else          { m_buffer = nullptr; }
         m_nrow = nrow;
         m_ncol = ncol;
@@ -136,7 +93,7 @@ private:
 
     size_t m_nrow = 0;
     size_t m_ncol = 0;
-
+    double * m_buffer = nullptr;
 };
 
 /*
@@ -215,10 +172,10 @@ Matrix multiply_mkl(Matrix const & mat1, Matrix const & mat2)
     
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 mat1.nrow(), mat2.ncol(), mat1.ncol(), 1.0, 
-                mat1.m_buffer, mat1.ncol(),  // A matrix
-                mat2.m_buffer, mat2.ncol(),  // B matrix
+                mat1.buffer(), mat1.ncol(),  // A matrix
+                mat2.buffer(), mat2.ncol(),  // B matrix
                 0.0, 
-                ret.m_buffer, ret.ncol());  // C matrix (result)
+                ret.buffer(), ret.ncol());  // C matrix (result)
 
     return ret;
 }
@@ -239,6 +196,7 @@ std::ostream & operator << (std::ostream & ostr, Matrix const & mat)
 
 
 
+
 PYBIND11_MODULE(_matrix, m){
     m.def("multiply_naive", &multiply_naive, "use naive");
     m.def("multiply_mkl", &multiply_mkl, "use mkl");
@@ -251,6 +209,7 @@ PYBIND11_MODULE(_matrix, m){
         .def("__getitem__", [](Matrix &self, std::pair<size_t, size_t> i) {
             return self(i.first, i.second);
         })
+        .def("__eq__", &Matrix::operator==)
         .def_property("nrow", &Matrix::nrow, nullptr)
         .def_property("ncol", &Matrix::ncol, nullptr);
 }

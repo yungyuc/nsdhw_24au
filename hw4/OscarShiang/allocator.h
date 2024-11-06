55a66cbcaa678481a3dca7834348383bae356f65
Oscar Shiang <oscar0225@livemail.tw>
2024-11-06 21:06:24 +0800

    Introduce atomic counters for memory accouting

--- hw4/OscarShiang/allocator.h
+++ hw4/OscarShiang/allocator.h
@@ -10,42 +10,44 @@ struct CustomAllocator
 {
     using value_type = T;
 
-    CustomAllocator()
-    {
-        allocated = 0;
-        deallocated = 0;
-        refcnt = 0;
-    }
+    CustomAllocator() = default;
 
     T *allocate(size_t n)
     {
-        size_t bytes = n * sizeof(T);
-        m_buf = static_cast<T *>(std::malloc(bytes));
+        size_t size = n * sizeof(T);
+        m_buf = static_cast<T *>(std::malloc(size));
         if (!m_buf) {
             throw std::bad_alloc();
         }
-
-        allocated += bytes;
+        m_allocated += size;
         return m_buf;
     }
 
     void deallocate(T *ptr, size_t n) noexcept
     {
+        size_t size = n * sizeof(T);
         if (ptr) {
-            size_t bytes = n * sizeof(T);
             std::free(ptr);
-            deallocated += bytes;
             if (ptr == m_buf) {
                 m_buf = NULL;
             }
         }
+        m_deallocated += size;
     }
 
     T *m_buf = NULL;
 
-    size_t allocated = 0;
-    size_t deallocated = 0;
-    size_t refcnt = 0;
+    static size_t allocated() { return m_allocated; }
+    static size_t deallocated() { return m_deallocated; }
+    static size_t bytes() { return m_allocated - m_deallocated; }
+
+    static std::atomic_size_t m_allocated;
+    static std::atomic_size_t m_deallocated;
+    static std::atomic_size_t m_refcnt;
 };
+
+template <class T> std::atomic_size_t CustomAllocator<T>::m_allocated(0);
+template <class T> std::atomic_size_t CustomAllocator<T>::m_deallocated(0);
+template <class T> std::atomic_size_t CustomAllocator<T>::m_refcnt(0);
 
 #endif /* __ALLOCATOR_H__ */

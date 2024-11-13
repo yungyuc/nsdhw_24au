6434a2868e050dd1e00b76eabcf6fe04466c09cd
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-11-13 20:08:11 +0800

    Complete HW4

--- hw4/lebr0nli/_matrix.h
+++ hw4/lebr0nli/_matrix.h
@@ -6,32 +6,57 @@
 #include <utility>
 #include <vector>
 
+template <typename T> class Mallocator {
+public:
+  using value_type = T;
+  static inline size_t bytes_allocated = 0;
+  static inline size_t total_allocated = 0;
+  static inline size_t total_deallocated = 0;
+
+  Mallocator() noexcept {}
+  template <typename U> Mallocator(const Mallocator<U> &) noexcept {}
+
+  T *allocate(std::size_t n);
+
+  void deallocate(T *p, std::size_t n) noexcept;
+};
+
+template <typename T, typename U>
+bool operator==(const Mallocator<T> &, const Mallocator<U> &) {
+  return true;
+}
+
+template <typename T, typename U>
+bool operator!=(const Mallocator<T> &, const Mallocator<U> &) {
+  return false;
+}
+
 struct Matrix {
-    size_t rows;
-    size_t cols;
-    std::vector<double> data;
+  size_t rows;
+  size_t cols;
+  std::vector<double, Mallocator<double>> data;
 
-    Matrix(size_t rows, size_t cols);
-    Matrix(const Matrix &other);
-    Matrix(Matrix &&other) noexcept;
-    Matrix &operator=(const Matrix &other);
-    Matrix &operator=(Matrix &&other) noexcept;
-    ~Matrix() = default;
+  Matrix(size_t rows, size_t cols);
+  Matrix(const Matrix &other);
+  Matrix(Matrix &&other) noexcept;
+  Matrix &operator=(const Matrix &other);
+  Matrix &operator=(Matrix &&other) noexcept;
+  ~Matrix() = default;
 
-    inline double &operator()(size_t row, size_t col);
-    inline const double &operator()(size_t row, size_t col) const;
-    bool operator==(const Matrix &other) const;
+  inline double &operator()(size_t row, size_t col);
+  inline const double &operator()(size_t row, size_t col) const;
+  bool operator==(const Matrix &other) const;
 
-    size_t nrow() const { return rows; }
-    size_t ncol() const { return cols; }
+  size_t nrow() const { return rows; }
+  size_t ncol() const { return cols; }
 };
 
 inline double &Matrix_at(Matrix &m, size_t i, size_t j) {
-    return m.data[i * m.cols + j];
+  return m.data[i * m.cols + j];
 }
 
 inline const double &Matrix_at(const Matrix &m, size_t i, size_t j) {
-    return m.data[i * m.cols + j];
+  return m.data[i * m.cols + j];
 }
 
 double Matrix_getitem(const Matrix &m,

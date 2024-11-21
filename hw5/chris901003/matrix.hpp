7a502e496b351a31c69fbe6f88989648f5796b45
chris901003 <a0987999103@gmail.com>
2024-11-21 09:29:31 +0800

    <nsdhw_24au> feat: first attempt

---
+++ hw5/chris901003/matrix.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class Matrix {
+    private:
+    size_t m_nrow, m_ncol;
+    double *data;
+    public:
+    Matrix(size_t nrow, size_t ncol);
+    Matrix(size_t nrow, size_t ncol, double init);
+    Matrix(size_t nrow, size_t ncol, vector<double> &init);
+    Matrix(const Matrix &m);
+    ~Matrix();
+
+    size_t nrow() const;
+    size_t ncol() const;
+    size_t index(size_t i, size_t j) const;
+    double* getData() const;
+    vector<double> getVector() const;
+
+    double operator()(size_t i, size_t j) const;
+    double& operator()(size_t i, size_t j);
+    bool operator==(const Matrix &m) const;
+};
+
+Matrix multiply_naive(const Matrix &m1, const Matrix &m2);
+Matrix multiply_tile(const Matrix &m1, const Matrix &m2, size_t tile_size);
+Matrix multiply_mkl(const Matrix &m1, const Matrix &m2);

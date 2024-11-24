805bb968a7c8b22f05293109549e059df4151a5d
Kang-Ding-Xin-NYCU <kang313552040.cs13@nycu.edu.tw>
2024-11-24 21:44:43 +0800

    second attempt

--- hw5/Kang-Ding-Xin-NYCU/matrix.cpp
+++ hw5/Kang-Ding-Xin-NYCU/matrix.cpp
@@ -1,103 +1,19 @@
 #include "matrix.hpp"
 
-Matrix::Matrix(size_t nrow, size_t ncol) : m_nrow(nrow), m_ncol(ncol)
+Matrix multiply_naive(Matrix const & m1, Matrix const & m2)
 {
-    reset_buffer(nrow, ncol);
-}
-
-Matrix::Matrix(Matrix const & other) : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
-{
-    reset_buffer(other.m_nrow, other.m_ncol);
-    for (size_t i=0; i<m_nrow; ++i)
+    if(m1.ncol()!=m2.nrow())
     {
-        for (size_t j=0; j<m_ncol; ++j)
-        {
-            (*this)(i,j) = other(i,j);
-        }
-    }
-}
-
-double Matrix::operator() (size_t row, size_t col) const
-{
-    return m_buffer[index(row, col)];
-}
-
-double & Matrix::operator() (size_t row, size_t col)
-{
-    return m_buffer[index(row, col)];
-}
-
-bool Matrix::operator==(Matrix const & other) const
-{
-    if ((this->m_nrow != other.nrow()) || (this->m_ncol != other.ncol())){
-        return false;
-    }
-    for(size_t i = 0; i < other.nrow(); i++){
-        for(size_t j = 0; j < other.ncol(); j++){
-            if(this->m_buffer[index(i, j)] != other(i, j)){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-double* Matrix::buf_get() const 
-{ 
-    return m_buffer;
-}
-
-void Matrix::buf_set(std::vector<double> &data) 
-{ 
-    for(size_t i = 0; i < m_nrow*m_ncol; i++)
-    {
-        m_buffer[i] = data[i];
-    }
-}
-
-size_t Matrix::nrow() const { return m_nrow; }
-size_t Matrix::ncol() const { return m_ncol; }
-size_t Matrix::size() const { return m_nrow * m_ncol; }
-
-void Matrix::printm(){
-    std::cout << m_nrow << " " << m_ncol << "\n";
-    for(size_t i = 0; i < m_nrow; ++i){
-        for(size_t j = 0; j < m_ncol; ++j){
-            std::cout << m_buffer[index(i, j)] << " ";
-        }
-        std::cout << "\n";
-    }
-}
-
-size_t Matrix::index(size_t row, size_t col) const
-{
-    return row * m_ncol + col;
-}
-
-void Matrix::reset_buffer(size_t nrow, size_t ncol)
-{
-    if (m_buffer) { delete[] m_buffer; }
-    const size_t nelement = nrow * ncol;
-    if (nelement) {
-        m_buffer = new double[nelement];
-        for(size_t i = 0; i < nelement; i++){
-            m_buffer[i] = 0;
-        }
-    }
-    else          { m_buffer = nullptr; }
-    m_nrow = nrow;
-    m_ncol = ncol;
-}
-
-Matrix multiply_naive(Matrix const & m1, Matrix const & m2){
-    if(m1.ncol()!=m2.nrow()){
         throw std::invalid_argument("wrong dimension");
     }
     Matrix res(m1.nrow(), m2.ncol());
-    for(size_t i = 0; i < m1.nrow(); ++i){
-        for(size_t j = 0; j < m2.ncol(); ++j){
+    for(size_t i = 0; i < m1.nrow(); ++i)
+    {
+        for(size_t j = 0; j < m2.ncol(); ++j)
+        {
             double element = 0;
-            for(size_t k = 0; k < m1.ncol(); ++k){
+            for(size_t k = 0; k < m1.ncol(); ++k)
+            {
                 element += m1(i, k) * m2(k, j);
             }
             res(i, j) = element;
@@ -106,21 +22,29 @@ Matrix multiply_naive(Matrix const & m1, Matrix const & m2){
     return res;
 }
 
-Matrix multiply_tile(Matrix const & m1, Matrix const & m2, size_t t){
-    if(m1.ncol() != m2.nrow()){
+Matrix multiply_tile(Matrix const & m1, Matrix const & m2, size_t t)
+{
+    if(m1.ncol() != m2.nrow())
+    {
         throw std::invalid_argument("wrong dimension");
     }
     Matrix res(m1.nrow(), m2.ncol());
-    for(size_t i = 0; i < m1.nrow(); i += t){
-        for(size_t j = 0; j < m2.ncol(); j += t){
-            for(size_t k = 0; k < m1.ncol(); k += t){
-                for(size_t it = i; it < std::min(m1.nrow(), i + t); ++it){
-                    for(size_t jt = j; jt < std::min(m2.ncol(), j + t); ++jt){
-                        double sum = 0.0;
-                        for(size_t kt = k; kt < std::min(m1.ncol(), k + t); ++kt){
-                            sum += m1(it, kt) * m2(kt, jt);
+    for(size_t i = 0; i < m1.nrow(); i += t)
+    {
+        for(size_t j = 0; j < m2.ncol(); j += t)
+        {
+            for(size_t k = 0; k < m1.ncol(); k += t)
+            {
+                for(size_t it = i; it < std::min(m1.nrow(), i + t); ++it)
+                {
+                    for(size_t jt = j; jt < std::min(m2.ncol(), j + t); ++jt)
+                    {
+                        double element = 0.0;
+                        for(size_t kt = k; kt < std::min(m1.ncol(), k + t); ++kt)
+                        {
+                            element += m1(it, kt) * m2(kt, jt);
                         }
-                        res(it, jt) += sum;
+                        res(it, jt) += element;
                     }
                 }
             }
@@ -129,9 +53,11 @@ Matrix multiply_tile(Matrix const & m1, Matrix const & m2, size_t t){
     return res;
 }
 
-Matrix multiply_mkl(Matrix const & m1, Matrix const & m2){
-    if(m1.ncol()!=m2.nrow()){
-        throw std::invalid_argument("wrong dimention");
+Matrix multiply_mkl(Matrix const & m1, Matrix const & m2)
+{
+    if(m1.ncol()!=m2.nrow())
+    {
+        throw std::invalid_argument("wrong dimension");
     }
     Matrix res(m1.nrow(), m2.ncol());
     size_t m = m1.nrow();
--- hw5/Kang-Ding-Xin-NYCU/matrix.hpp
+++ hw5/Kang-Ding-Xin-NYCU/matrix.hpp
@@ -10,35 +10,93 @@
 #include <mkl.h>
 #include <vector>
 
-class Matrix {
+class Matrix 
+{
 
 public:
 
-    Matrix(size_t nrow, size_t ncol);
+    Matrix(size_t nrow, size_t ncol)
+      : m_nrow(nrow), m_ncol(ncol)
+    {
+        reset_buffer(nrow, ncol);
+    }
 
-    Matrix(Matrix const & other);
+    Matrix(Matrix const & other)
+      : m_nrow(other.m_nrow), m_ncol(other.m_ncol)
+    {
+        reset_buffer(other.m_nrow, other.m_ncol);
+        for (size_t i=0; i<m_nrow; ++i)
+        {
+            for (size_t j=0; j<m_ncol; ++j)
+            {
+                (*this)(i,j) = other(i,j);
+            }
+        }
+    }
 
-    double   operator() (size_t row, size_t col) const;
+    double   operator() (size_t row, size_t col) const
+    {
+        return m_buffer[index(row, col)];
+    }
 
-    double & operator() (size_t row, size_t col);
+    double & operator() (size_t row, size_t col)
+    {
+        return m_buffer[index(row, col)];
+    }
 
-    bool operator==(Matrix const & other) const;
-    double* buf_get() const;
+    bool operator==(Matrix const & other) const 
+    {
+        if ((this->m_nrow != other.nrow()) || (this->m_ncol != other.ncol())){
+            return false;
+        }
+        for(size_t i = 0; i < other.nrow(); i++){
+            for(size_t j = 0; j < other.ncol(); j++){
+                if(this->m_buffer[index(i, j)] != other(i, j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    void buf_set(std::vector<double> &data);
+    double* buf_get() const 
+    { 
+        return m_buffer;
+    }
 
-    size_t nrow() const;
-    size_t ncol() const;
-    size_t size() const;
+    void   buf_set(std::vector<double> &data) 
+    { 
+        for(size_t i = 0; i < m_nrow*m_ncol; i++)
+        {
+            m_buffer[i] = data[i];
+        }
+    }
 
-    void printm();
-    
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
+    size_t size() const { return m_nrow * m_ncol; }
 
 private:
 
-    size_t index(size_t row, size_t col) const;
+    size_t index(size_t row, size_t col) const
+    {
+        return row * m_ncol + col;
+    }
 
-    void reset_buffer(size_t nrow, size_t ncol);
+    void reset_buffer(size_t nrow, size_t ncol)
+    {
+        if (m_buffer) { delete[] m_buffer; }
+        const size_t nelement = nrow * ncol;
+        if (nelement) {
+            m_buffer = new double[nelement];
+            for(size_t i = 0; i < nelement; i++){
+                m_buffer[i] = 0;
+            }
+        }
+        else          { m_buffer = nullptr; }
+        m_nrow = nrow;
+        m_ncol = ncol;
+    }
 
     size_t m_nrow = 0;
     size_t m_ncol = 0;
